928ef80b660e780b6139d8c1d41e3206c5c65f44
Swayamagg <swayamaggarwal777@gmail.com>
2025-09-14 15:42:24 +0530

    create hero page

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -4,13 +4,13 @@ import "../public/styles/index.css"
 function Navbar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary" style={{backgroundColor:"#fff"}}>
-        <div class="container">
-          <a class="navbar-brand p-2" href="#">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{backgroundColor:"#fff"}}>
+        <div className="container">
+          <a className="navbar-brand p-2" href="#">
             <img src="/images/logo.svg" alt="logo_img" style={{width:"25%"}}/>
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -18,33 +18,33 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <form class="d-flex" role="search">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="#">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <form className="d-flex" role="search">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">
                     Signup
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
                     About
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="#">
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">
                     Products
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="#">
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">
                     Pricing
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="#">
+                <li className="nav-item">
+                  <a className="nav-link " aria-current="page" href="#">
                     Support
                   </a>
                 </li>
