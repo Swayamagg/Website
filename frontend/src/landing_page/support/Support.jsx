9c806c952f98667a0d237bf1e5d014a221314dd6
Swayamagg <swayamaggarwal777@gmail.com>
2025-09-19 14:35:34 +0530

    chnages in support section

---
+++ frontend/src/landing_page/support/Support.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+
+function Support() {
+  return (
+    <div class="accordion" id="accordionExample" style={{width:"800px",marginLeft:"9.5rem"}}>
+      <div class="accordion-item mt-5">
+        <h2 class="accordion-header" id="headingOne">
+          <button
+            class="accordion-button collapsed d-flex justify-content-between align-items-center"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+          >
+            Account Opening
+            <i class="bi bi-chevron-down ms-auto chevron"></i>
+          </button> 
+        </h2>
+        <div
+          id="collapseOne"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <ul style={{fontSize:"17px",lineHeight:"2.7"}}>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Resident individual</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Minor</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Non Resident Indian (NRI)</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Company, Partnership, HUF and LLP</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Glossary</li></a>
+           </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="accordion-item mt-4 border-top">
+        <h2 class="accordion-header" id="headingTwo">
+          <button
+            class="accordion-button collapsed d-flex justify-content-between align-items-center"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseTwo"
+          >
+            Your Zerodha Account
+            <i class="bi bi-chevron-down ms-auto chevron"></i>
+          </button>
+        </h2>
+        <div
+          id="collapseTwo"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+             <ul style={{fontSize:"17px",lineHeight:"2.7"}}>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Your Profile</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Account modification</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Client Master Report (CMR) and Depository Participant (DP)</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Nomination</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Transfer and conversion of securities</li></a>
+           </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="accordion-item mt-4 border-top">
+        <h2 class="accordion-header" id="headingThree">
+          <button
+            class="accordion-button collapsed d-flex justify-content-between align-items-center"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseThree"
+          >
+            Kite
+            <i class="bi bi-chevron-down ms-auto chevron"></i>
+          </button>
+        </h2>
+        <div
+          id="collapseThree"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+             <ul style={{fontSize:"17px",lineHeight:"2.7"}}>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>IPO</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Trading FAQs</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Margin Trading Facility (MTF) and Margins</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Charts and orders</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Alerts and Nudges</li></a> 
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>General</li></a> 
+           </ul>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item mt-4 border-top">
+        <h2 class="accordion-header" id="headingFour">
+          <button
+            class="accordion-button collapsed d-flex justify-content-between align-items-center"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseFour"
+          >
+            Funds
+            <i class="bi bi-chevron-down ms-auto chevron"></i>
+          </button>
+        </h2>
+        <div
+          id="collapseFour"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+          <ul style={{fontSize:"17px",lineHeight:"2.7"}}>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Add money</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Withdraw money</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Add bank accounts</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>eMandates</li></a>
+           </ul>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item mt-4 border-top">
+        <h2 class="accordion-header" id="headingFive">
+          <button
+            class="accordion-button collapsed d-flex justify-content-between align-items-center"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseFive"
+          >
+             Console 
+            <i class="bi bi-chevron-down ms-auto chevron"></i>
+          </button>
+        </h2>
+        <div
+          id="collapseFive"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body"></div>
+           <ul style={{fontSize:"17px",lineHeight:"2.7"}}>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Portfolio</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Corporate actions</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Funds statement</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Reports</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Profile</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Segments</li></a>
+           </ul>
+        </div>
+      </div>
+      <div class="accordion-item mt-4 mb-5 border-top">
+        <h2 class="accordion-header" id="headingSix">
+          <button
+            class="accordion-button collapsed d-flex justify-content-between align-items-center"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseSix"
+          >
+            Coin
+            <i class="bi bi-chevron-down ms-auto chevron"></i>
+          </button>
+        </h2>
+        <div
+          id="collapseSix"
+          class="accordion-collapse collapse"
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+             <ul style={{fontSize:"17px",lineHeight:"2.7"}}>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Mutual Funds</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>National Pension Scheme(NPS)</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Fixed Deposit(FD)</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Features on Coin</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>Payments and Orders</li></a>
+              <a href="" style={{textDecoration:"none",color:"#397DD0"}}><li>General</li></a>
+           </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Support;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import NotFound from './landing_page/NotFound.jsx';
 import AboutPage from './landing_page/about/AboutPage.jsx';
 import ProductPage from './landing_page/products/ProductPage.jsx';
 import Pricing from './landing_page/pricing/Pricing.jsx';
+import Support from './landing_page/support/Support.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
     <Route path='/about' element={<AboutPage/>}/>
     <Route path='/products' element={<ProductPage/>}/>
     <Route path='/pricing' element={<Pricing/>}/>
+    <Route path='/support' element={<Support/>}/>
     <Route path='*' element={<NotFound/>}/>
   </Routes>
    <Footer/>
