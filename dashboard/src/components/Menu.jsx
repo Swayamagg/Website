d6382775428f811a4cb555cfe74c21d3e5c576a5
Swayamagg <swayamaggarwal777@gmail.com>
2025-09-27 23:28:24 +0530

    changes in holdings

--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -1,28 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {Link} from "react-router-dom";
+import "../App.css"
 const Menu = () => {
+  const [selectMenu,setSelectMenu]=useState(0);
+  const [isProfile,setProfile]=useState(false);
+
+  const handleMenu=()=>{
+    setSelectMenu(selectMenu);
+  }
+  const handleProfile=()=>{
+    setProfile(!isProfile);
+  }
+  const menuClass="menu";
+  const activeMenu="menu selected";
   return (
     <div className="menu-container">
       <img src="logo.png" style={{ width: "50px" }} />
       <div className="menus">
         <ul>
           <li>
-            <p>Dashboard</p>
+            <Link onClick={()=>handleMenu(0)} to="/" style={{textDecoration:"none"}}><p className={selectMenu===0 ? activeMenu:menuClass}>Dashboard</p></Link>
           </li>
           <li>
-            <p>Orders</p>
+            <Link onClick={()=>handleMenu(1)} to="/orders" style={{textDecoration:"none"}}><p className={selectMenu===0 ? activeMenu:menuClass}>Orders</p></Link>
           </li>
           <li>
-            <p>Holdings</p>
+            <Link onClick={()=>handleMenu(2)} to="/holdings" style={{textDecoration:"none"}}><p className={selectMenu===0 ? activeMenu:menuClass}>Holdings</p></Link>
           </li>
           <li>
-            <p>Positions</p>
+           <Link onClick={()=>handleMenu(3)} to="/positions" style={{textDecoration:"none"}}><p className={selectMenu===0 ? activeMenu:menuClass}>Positions</p></Link>
           </li>
           <li>
-            <p>Funds</p>
+            <Link onClick={()=>handleMenu(4)} to="/funds" style={{textDecoration:"none"}}><p className={selectMenu===0 ? activeMenu:menuClass}>Funds</p></Link>
           </li>
           <li>
-            <p>Apps</p>
+            <Link onClick={()=>handleMenu(5)} to="/apps" style={{textDecoration:"none"}}><p className={selectMenu===0 ? activeMenu:menuClass}>Apps</p></Link>
           </li>
         </ul>
         <hr />
